23ae1703ea50224f243d83f2eac7e501a6b13e44
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-12-10 23:49:59 +0530

    day 5 part a solved

--- 05/main.go
+++ 05/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const SPACE_CHAR_CODE = 32
@@ -29,8 +31,7 @@ func parseStack(lines []string) (int, [][]string) {
 		// 3 - 9 // 1 + 4*2
 		// 4 - 13  // 1 + 4*3
 		for s := 0; s < n; s++ {
-			// if got space then increment the value
-			// otherwise if we found an element
+			// check whether the  values are not space or [ or ]
 			var pos = 1 + 4*s
 			var char = lines[i][pos]
 			if char != 32 && char != '[' && char != ']' {
@@ -39,6 +40,33 @@ func parseStack(lines []string) (int, [][]string) {
 		}
 	}
 	return n, parsed
+}
+
+type Ins struct {
+	n    int
+	from int
+	to   int
+}
+
+func parseIns(lines []string) []Ins {
+	var ins []Ins
+	// split the string by spaces
+	for _, k := range lines {
+		var sp = strings.Split(k, " ")
+		if len(sp) != 6 {
+			panic("invalid instruction string got")
+		}
+		n, _ := strconv.Atoi(sp[1])
+		f, _ := strconv.Atoi(sp[3])
+		t, _ := strconv.Atoi(sp[5])
+		i := Ins{
+			from: f,
+			to:   t,
+			n:    n,
+		}
+		ins = append(ins, i)
+	}
+	return ins
 }
 
 func get() ([]string, []string) {
@@ -55,6 +83,7 @@ func get() ([]string, []string) {
 		line := scanner.Text()
 		if line == "" {
 			isIns = true
+			continue
 		}
 		if !isIns {
 			stack = append(stack, line)
@@ -65,7 +94,32 @@ func get() ([]string, []string) {
 	return stack, ins
 }
 
+func partA(stack [][]string, instructions []Ins, n int) string {
+	for _, ins := range instructions {
+		i, j := ins.from-1, ins.to-1
+		ssize := len(stack[i])
+		// s[start:end] [start, end)
+		// elements from start index to end-1
+		popped := stack[i][ssize-ins.n : ssize] // get the last n items
+		// update the source stack
+		stack[i] = stack[i][:ssize-ins.n]
+		for k := len(popped) - 1; k >= 0; k-- {
+			// as for a stack lifo -> last in first out
+			// so insert the last one first and so...on
+			stack[j] = append(stack[j], popped[k])
+		}
+	}
+	var res string = ""
+	for _, s := range stack {
+		res += s[len(s)-1]
+	}
+	return res
+}
+
 func main() {
-	stack, ins := get()
-	fmt.Println(stack, ins)
+	var stack, ins = get()
+	var n, stacks = parseStack(stack)
+	var mv = parseIns(ins)
+	var res = partA(stacks, mv, n)
+	fmt.Println(res)
 }
--- 05/main_test.go
+++ 05/main_test.go
@@ -35,4 +35,54 @@ func Test(t *testing.T) {
 			}
 		}
 	})
+
+	t.Run("should return the top crates in all stacks", func(t *testing.T) {
+		var stacks = [][]string{
+			{"Z", "N"},
+			{"M", "C", "D"},
+			{"P"},
+		}
+		var ins = []Ins{
+			{from: 2, to: 1, n: 1},
+			{from: 1, to: 3, n: 3},
+			{from: 2, to: 1, n: 2},
+			{from: 1, to: 2, n: 1},
+		}
+		var got = partA(stacks, ins, len(stacks))
+		var want = "CMZ"
+		if got != want {
+			t.Errorf("got - %s, want - %s", got, want)
+		}
+	})
+
+	t.Run("should parse the movement instruction correctly", func(t *testing.T) {
+		var input = []string{
+			"move 1 from 2 to 1",
+			"move 3 from 1 to 3",
+			"move 2 from 2 to 1",
+			"move 1 from 1 to 2",
+		}
+
+		var got = parseIns(input)
+		var want = []Ins{
+			{from: 2, to: 1, n: 1},
+			{from: 1, to: 3, n: 3},
+			{from: 2, to: 1, n: 2},
+			{from: 1, to: 2, n: 1},
+		}
+
+		if len(got) != len(want) {
+			t.Error("length does not match")
+		}
+
+		for i := 0; i < len(got); i++ {
+			var g, w = got[i], want[i]
+			if g.from != w.from || g.to != w.to || g.n != w.n {
+				t.Errorf("mismatch got and want")
+				t.Error("got", g)
+				t.Error("want", w)
+				t.FailNow()
+			}
+		}
+	})
 }
